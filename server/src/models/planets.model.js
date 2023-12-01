d573341d094b9bd3e963f631ebf32aa7bb4ba462
James Takaendisa <james.takaendisa01@gmail.com>
2023-12-01 18:51:18 +0200

    Set up planets api

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
+
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
+};
+
+fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+  .pipe(parse({ comment: '#', columns: true }))
+  .on('data', (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log(habitablePlanets.map((planet) => planet['kepler_name']));
+    console.log(`${habitablePlanets.length} habitable planets found!`);
+  });
+
+module.exports = habitablePlanets;
