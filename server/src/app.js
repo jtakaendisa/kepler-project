609ce33f9198324e1b584ad29148230d469d6494
James Takaendisa <james.takaendisa01@gmail.com>
2023-12-04 18:00:29 +0200

    Set up tests for launches API endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 const launchesRouter = require('./routes/launches/launches.router');
@@ -13,7 +13,7 @@ app.use(
     origin: 'http://localhost:3000',
   })
 );
-// app.use(morgan('combined'));
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
