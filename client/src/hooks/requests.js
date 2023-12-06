7ef63a3ad0c6e3523a8005a756a4d6feea7d724e
James Takaendisa <james.takaendisa01@gmail.com>
2023-12-06 16:35:41 +0200

    refactor: version api routes

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -2,13 +2,13 @@ const API_URL = 'http://localhost:8000';
 
 //Load planets and return as JSON
 async function httpGetPlanets() {
-  const response = await fetch(`${API_URL}/planets`);
+  const response = await fetch(`${API_URL}/v1/planets`);
   return await response.json();
 }
 
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  const response = await fetch(`${API_URL}/launches`);
+  const response = await fetch(`${API_URL}/v1/launches`);
   const fetchedLaunches = await response.json();
   return fetchedLaunches.sort((a, b) => a.flightNumber - b.flightNumber);
 }
@@ -16,7 +16,7 @@ async function httpGetLaunches() {
 // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
   try {
-    return await fetch(`${API_URL}/launches`, {
+    return await fetch(`${API_URL}/v1/launches`, {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
@@ -31,7 +31,7 @@ async function httpSubmitLaunch(launch) {
 // Delete launch with given ID.
 async function httpAbortLaunch(id) {
   try {
-    return await fetch(`${API_URL}/launches/${id}`, {
+    return await fetch(`${API_URL}/v1/launches/${id}`, {
       method: 'delete',
     });
   } catch (err) {
