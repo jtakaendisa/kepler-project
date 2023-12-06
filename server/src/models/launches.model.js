20c06b3d04bba626f4030b224ae744418768812c
James Takaendisa <james.takaendisa01@gmail.com>
2023-12-06 13:40:03 +0200

    set up deleting launches using Mongo

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -14,8 +14,10 @@ const launch = {
   success: true,
 };
 
-function existsLaunchWithId(launchId) {
-  return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+  return await launches.findOne({
+    flightNumber: launchId,
+  });
 }
 
 async function getLatestFlightNumber() {
@@ -71,11 +73,18 @@ async function scheduleNewLaunch(launch) {
   await saveLaunch(newLaunch);
 }
 
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId);
-  aborted.upcoming = false;
-  aborted.success = false;
-  return aborted;
+async function abortLaunchById(launchId) {
+  const aborted = await launches.updateOne(
+    {
+      flightNumber: launchId,
+    },
+    {
+      upcoming: false,
+      success: false,
+    }
+  );
+
+  return aborted.matchedCount === 1 && aborted.modifiedCount === 1;
 }
 
 module.exports = {
